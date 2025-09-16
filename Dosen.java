a635ac54eb9a2988c5f4da220d18936608a499bd
linggarsy <linggarsulisyanti@gmail.com>
2025-09-16 14:23:01 +0700

    inisiasi class Dosen

---
+++ Dosen.java
@@ -0,0 +1,12 @@
+public class Dosen {
+    String namaDosen;
+    int nidnDosen;
+
+    void mengampuMataKuliah(){
+        System.out.println("PBO");
+    }
+    
+    public String mengerjakanPenelitian(){
+        return "dosen mengerjakan penelitian";
+    }
+}
